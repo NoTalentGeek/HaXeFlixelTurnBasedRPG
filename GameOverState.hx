ef3dfe78c18959e054a94b93ff9f4af39396b453
notalentgeek <mikael.pratama@yahoo.com>
2016-02-22 11:22:19 +0100

    Full fix on GameOverState.hx.

--- GameOverState.hx
+++ GameOverState.hx
@@ -1,5 +1,10 @@
 package;
 
+
+
+
+
+
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
@@ -9,20 +14,38 @@ import flixel.util.FlxAxes;
 import flixel.util.FlxColor;
 import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxSave;
+
+
+
+
+
 using flixel.util.FlxSpriteUtil;
+
+
+
+
 
 class GameOverState extends FlxState{
 
-    private var hiScoreTextObject       :FlxText;   /*Text to show the hi - score.*/
-    private var mainMenuButtonObject    :FlxButton; /*Button to go to main menu.*/
-    private var messageTextObject       :FlxText;   /*The final score message text.*/
-    private var scoreInt                :Int = 0;   /*Number of coins we have collected.*/
-    private var scoreSpriteObject       :FlxSprite; /*Sprite for a coin icon.*/
-    private var scoreTextObject         :FlxText;   /*Text of the score.*/
-    private var titleTextObject         :FlxText;   /*The title text.*/
-    private var winBool                 :Bool;      /*If we won or lost.*/
-    
-    /*Called from PlayState, this will set our win and score variables.
+
+
+
+
+    private var hiScoreFlxText      :FlxText;   /*Text to show the hi - score.*/
+    private var mainMenuFlxButton   :FlxButton; /*Button to go to main menu.*/
+    private var messageFlxText      :FlxText;   /*The final score message text.*/
+    private var scoreInt            :Int = 0;   /*Number of coins we have collected.*/
+    private var scoreFlxSprite      :FlxSprite; /*Sprite for a coin icon.*/
+    private var scoreFlxText        :FlxText;   /*Text of the score.*/
+    private var titleFlxText        :FlxText;   /*The title text.*/
+    private var winBool             :Bool;      /*If we won or lost.*/
+
+
+
+
+
+    /*==================================================
+    Called from PlayState, this will set our win and score variables.
     _winBool true if the player beat the boss, false if they died.
     _scoreInt the number of coins collected.*/
     public function new(_winBool:Bool, _scoreInt:Int){
@@ -32,7 +55,13 @@ class GameOverState extends FlxState{
         super();
 
     }
-    
+    /*==================================================*/
+
+
+
+
+
+    ==================================================
     override public function create():Void{
         
         #if !FLX_NO_MOUSE
@@ -43,74 +72,74 @@ class GameOverState extends FlxState{
         
 
 
-        titleTextObject = new FlxText(
+        titleFlxText = new FlxText(
             0, 20, 0,
             winBool ? "You win!" : "Game Over!",
             22
         );
-        titleTextObject.alignment = CENTER;
-        titleTextObject.screenCenter(FlxAxes.X);
-        add(titleTextObject);
+        titleFlxText.alignment = CENTER;
+        titleFlxText.screenCenter(FlxAxes.X);
+        add(titleFlxText);
         
 
 
-        messageTextObject = new FlxText(
+        messageFlxText = new FlxText(
             0,
             (FlxG.height / 2) - 18,
             0,
             "Final score: ",
             8
         );
-        messageTextObject.alignment = CENTER;
-        messageTextObject.screenCenter(FlxAxes.X);
-        add(messageTextObject);
+        messageFlxText.alignment = CENTER;
+        messageFlxText.screenCenter(FlxAxes.X);
+        add(messageFlxText);
         
 
 
-        scoreSpriteObject = new FlxSprite(
+        scoreFlxSprite = new FlxSprite(
             (FlxG.width/ 2) - 8,
             0,
             AssetPaths.coin__png
         );
-        scoreSpriteObject.screenCenter(FlxAxes.Y);
-        add(scoreSpriteObject);
+        scoreFlxSprite.screenCenter(FlxAxes.Y);
+        add(scoreFlxSprite);
         
 
 
-        scoreTextObject = new FlxText(
+        scoreFlxText = new FlxText(
             (FlxG.width/ 2),
             0, 0,
             Std.string(scoreInt),
             8
         );
-        scoreTextObject.screenCenter(FlxAxes.Y);
-        add(scoreTextObject);
+        scoreFlxText.screenCenter(FlxAxes.Y);
+        add(scoreFlxText);
         
 
 
         var _hiScore = CheckHighScoreInt(scoreInt);
-        hiScoreTextObject = new FlxText(
+        hiScoreFlxText = new FlxText(
             0,
             (FlxG.height / 2) + 10,
             0,
             "Hi - score: " + _hiScore,
             8
         );
-        hiScoreTextObject.alignment = CENTER;
-        hiScoreTextObject.screenCenter(FlxAxes.Y);
-        add(hiScoreTextObject);
+        hiScoreFlxText.alignment = CENTER;
+        hiScoreFlxText.screenCenter(FlxAxes.Y);
+        add(hiScoreFlxText);
         
 
 
-        mainMenuButtonObject = new FlxButton(
+        mainMenuFlxButton = new FlxButton(
             0,
             FlxG.height - 32,
             "Main menu.",
-            ChangeStateToMainMenuVoid
+            ClickMainMenuVoid
         );
-        mainMenuButtonObject.screenCenter(FlxAxes.X);
-        mainMenuButtonObject.onUp.sound = FlxG.sound.load(AssetPaths.select__wav);
-        add(mainMenuButtonObject);
+        mainMenuFlxButton.screenCenter(FlxAxes.X);
+        mainMenuFlxButton.onUp.sound = FlxG.sound.load(AssetPaths.select__wav);
+        add(mainMenuFlxButton);
         
 
 
@@ -119,38 +148,50 @@ class GameOverState extends FlxState{
         super.create();
 
     }
-    
+    /*==================================================*/
+
+
+
+
+
+    /*==================================================*/
     override public function destroy():Void{
 
-        mainMenuButtonObject    = FlxDestroyUtil.destroy(mainMenuButtonObject);
-        messageTextObject       = FlxDestroyUtil.destroy(messageTextObject);
-        scoreSpriteObject       = FlxDestroyUtil.destroy(scoreSpriteObject);
-        scoreTextObject         = FlxDestroyUtil.destroy(scoreTextObject);
-        titleTextObject         = FlxDestroyUtil.destroy(titleTextObject);
+        mainMenuFlxButton    = FlxDestroyUtil.destroy(mainMenuFlxButton);
+        messageFlxText       = FlxDestroyUtil.destroy(messageFlxText);
+        scoreFlxSprite       = FlxDestroyUtil.destroy(scoreFlxSprite);
+        scoreFlxText         = FlxDestroyUtil.destroy(scoreFlxText);
+        titleFlxText         = FlxDestroyUtil.destroy(titleFlxText);
 
     }
-    
-    /*This function will compare the new score with the saved hi - score. 
+    /*==================================================*/
+
+
+
+
+
+    /*==================================================
+    This function will compare the new score with the saved hi - score. 
     If the new score is higher, it will save it as the new hi - score,
         otherwise, it will return the saved hi-score.*/
     private function CheckHighScoreInt(_scoreInt:Int):Int
     {
 
         var tempScoreInt    :Int        = _scoreInt;
-        var saveObject      :FlxSave    = new FlxSave();
+        var saveFlxSave     :FlxSave    = new FlxSave();
 
-        if(saveObject.bind("Flixel-Tutorial")){
+        if(saveFlxSave.bind("HaXeFlixelTurnBasedRPG")){
 
-            if(saveObject.data.hiscore != null){
+            if(saveFlxSave.data.hiscore != null){
 
-                if(saveObject.data.hiscore > tempScoreInt){
+                if(saveFlxSave.data.hiscore > tempScoreInt){
 
-                    tempScoreInt = saveObject.data.hiscore;
+                    tempScoreInt = saveFlxSave.data.hiscore;
 
                 }
                 else{
 
-                    saveObject.data.hiscore = tempScoreInt;
+                    saveFlxSave.data.hiscore = tempScoreInt;
 
                 }
 
@@ -158,19 +199,29 @@ class GameOverState extends FlxState{
 
         }
 
-        saveObject.close();
+        saveFlxSave.close();
 
         return tempScoreInt;
 
     }
-    
-    /*Animation when this program change back to main menu state.*/
-    private function ChangeStateToMainMenuVoid():Void{
+    /*==================================================*/
+
+
+
+
+    /*==================================================
+    Animation when this program change back to main menu state.*/
+    private function ClickMainMenuVoid():Void{
 
         FlxG.camera.fade(FlxColor.BLACK, .33, false, function(){
             FlxG.switchState(new MenuState());
         });
 
     }
-    
+    /*==================================================*/
+
+
+
+
+
 }
